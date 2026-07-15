0a0cb5459ebf4bc607ce0aa44bfc31a45954cf91
amitd1299 <dorwekar2000@gmail.com>
2026-07-15 16:50:33 +0530

    Add Terraform infra + Ansible playbooks

---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "amit-terraform-state-564186"
+    key            = "devops-platform/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
